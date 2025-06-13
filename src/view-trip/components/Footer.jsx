33269ed925f1b820589954303bd6c6389755bd72
Nishwitha <nishwitha.1032@gmail.com>
2025-06-13 13:45:43 -0500

    first commit

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='my-7'>
+        <h2 className='text-center text-gray-500'><b>AI QuickTrip planner</b> created by <b>Nishwitha Reddy</b></h2>
+    </div>
+  )
+}
+
+export default Footer
